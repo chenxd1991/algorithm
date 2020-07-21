7a4f75cc1c18171678678b7a9228c9350d244d59
xiaodong.chen <xiaodong.chen@huixiaoer.com>
2020-07-21 19:21:17 +0800

    数组

--- src/main/java/com/chenxd/algorithm/array/ArrayTraining.java
+++ src/main/java/com/chenxd/algorithm/array/ArrayTraining.java
@@ -1,6 +1,7 @@
 package com.chenxd.algorithm.array;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -27,12 +28,12 @@ public class ArrayTraining {
 //        }
 //        return arr;
 //    }
-    public static int[] runningSum(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
-        }
-        return nums;
-    }
+//    public static int[] runningSum(int[] nums) {
+//        for (int i = 1; i < nums.length; i++) {
+//            nums[i] += nums[i - 1];
+//        }
+//        return nums;
+//    }
 
     /**
      * 给你一个整数数组 nums 。
@@ -46,17 +47,17 @@ public class ArrayTraining {
      * 输出：6
      * 解释：数组中的每组数字都是好数对
      */
-    public static int numIdenticalPairs(int[] nums) {
-        int count = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+//    public static int numIdenticalPairs(int[] nums) {
+//        int count = 0;
+//        for (int i = 0; i < nums.length - 1; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] == nums[j]) {
+//                    count++;
+//                }
+//            }
+//        }
+//        return count;
+//    }
 
     /**
      * 给你一个数组 candies 和一个整数 extraCandies ，其中 candies[i] 代表第 i 个孩子拥有的糖果数目。
@@ -65,13 +66,242 @@ public class ArrayTraining {
      * 输入：candies = [2,3,5,1,3], extraCandies = 3
      * 输出：[true,true,true,false,true]
      */
-    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> list = new ArrayList<>();
+//    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+//        int max = Integer.MIN_VALUE;
+//        for (int candy : candies) {
+//            max = Math.max(max, candy);
+//        }
+//        List<Boolean> list = new ArrayList<>();
+//        for (int candy : candies) {
+//            if (candy + extraCandies >= max) {
+//                list.add(true);
+//            } else {
+//                list.add(false);
+//            }
+//        }
+//        return list;
+//    }
 
-        return list;
+    /**
+     * 给你一个数组 nums ，数组中有 2n 个元素，按 [x1,x2,...,xn,y1,y2,...,yn] 的格式排列。
+     * 请你将数组按 [x1,y1,x2,y2,...,xn,yn] 格式重新排列，返回重排后的数组。
+     * 示例 1： 0 2 4
+     * 输入：nums = [2,5,1,3,4,7], n = 3
+     * 输出：[2,3,5,4,1,7]
+     * 解释：由于 x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 ，所以答案为 [2,3,5,4,1,7]
+     */
+//    public static int[] shuffle(int[] nums, int n) {
+//        int[] arr = new int[2 * n];
+//        for (int i = 0; i < n; i++) {
+//            arr[2 * i] = nums[i];
+//            arr[2 * i + 1] = nums[n + i];
+//        }
+//        return arr;
+//    }
+
+    /**
+     * 给你两个整数，n 和 start 。
+     * 数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
+     * 请返回 nums 中所有元素按位异或（XOR）后得到的结果。
+     * 示例 1：
+     * 输入：n = 5, start = 0
+     * 输出：8
+     * 解释：数组 nums 为 [0, 2, 4, 6, 8]，其中 (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8 。
+     * "^" 为按位异或 XOR 运算符。
+     */
+//    public static int xorOperation(int n, int start) {
+//        int res = start;
+//        for (int i = 1; i < n; i++) {
+//            res ^= start + 2 * i;
+//        }
+//        return res;
+//    }
+
+    /**
+     * 给你两个整数数组 nums 和 index。你需要按照以下规则创建目标数组：
+     * 目标数组 target 最初为空。
+     * 按从左到右的顺序依次读取 nums[i] 和 index[i]，在 target 数组中的下标 index[i] 处插入值 nums[i] 。
+     * 重复上一步，直到在 nums 和 index 中都没有要读取的元素。
+     * 请你返回目标数组。
+     * 题目保证数字插入位置总是存在。
+     * 示例 1：
+     * 输入：nums = [0,1,2,3,4], index = [0,1,2,2,1]
+     * 输出：[0,4,1,3,2]
+     * 解释：
+     * nums       index     target
+     * 0            0        [0]
+     * 1            1        [0,1]
+     * 2            2        [0,1,2]
+     * 3            2        [0,1,3,2]
+     * 4            1        [0,4,1,3,2]
+     */
+//    public int[] createTargetArray(int[] nums, int[] index) {
+//        ArrayList<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            list.add(index[i], nums[i]);
+//        }
+//        return list.stream().mapToInt(Integer::intValue).toArray();
+//    }
+
+    /**
+     * 给你一个以行程长度编码压缩的整数列表 nums 。
+     * 考虑每对相邻的两个元素 [freq, val] = [nums[2*i], nums[2*i+1]] （其中 i >= 0 ），每一对都表示解压后子列表中有 freq 个值为 val 的元素，你需要从左到右连接所有子列表以生成解压后的列表。
+     * 请你返回解压后的列表。
+     * 示例：
+     * 输入：nums = [1,2,3,4]
+     * 输出：[2,4,4,4]
+     * 解释：第一对 [1,2] 代表着 2 的出现频次为 1，所以生成数组 [2]。
+     * 第二对 [3,4] 代表着 4 的出现频次为 3，所以生成数组 [4,4,4]。
+     * 最后将它们串联到一起 [2] + [4,4,4] = [2,4,4,4]。
+     */
+//    public static int[] decompressRLElist(int[] nums) {
+//        List<Integer> list = new ArrayList<>();
+//        for (int i = 0; i < nums.length / 2; i++) {
+//            for (int j = 0; j < nums[2 * i]; j++) {
+//                list.add(nums[2 * i + 1]);
+//            }
+//        }
+//        return list.parallelStream().mapToInt(Integer::intValue).toArray();
+//    }
+
+    /**
+     *  n 名士兵站成一排。每个士兵都有一个 独一无二 的评分 rating 。
+     * 每 3 个士兵可以组成一个作战单位，分组规则如下：
+     * 从队伍中选出下标分别为 i、j、k 的 3 名士兵，他们的评分分别为 rating[i]、rating[j]、rating[k]
+     * 作战单位需满足： rating[i] < rating[j] < rating[k] 或者 rating[i] > rating[j] > rating[k] ，其中  0 <= i < j < k < n
+     * 请你返回按上述条件可以组建的作战单位数量。每个士兵都可以是多个作战单位的一部分。
+     * 示例 1：
+     * 输入：rating = [2,5,3,4,1]
+     * 输出：3
+     * 解释：我们可以组建三个作战单位 (2,3,4)、(5,4,1)、(5,3,1) 。
+     * 示例 2：
+     * 输入：rating = [2,1,3]
+     * 输出：0
+     * 解释：根据题目条件，我们无法组建作战单位。
+     * 示例 3：
+     * 输入：rating = [1,2,3,4]
+     * 输出：4
+     */
+//    public static int numTeams(int[] rating) {
+//
+//    }
+
+    /**
+     * 给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
+     * 换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
+     * 以数组形式返回答案。
+     * 示例 1：
+     * 输入：nums = [8,1,2,2,3]
+     * 输出：[4,0,1,1,3]
+     * 输入：nums = [6,5,4,8]
+     * 输出：[2,1,0,3]
+     * 输入：nums = [7,7,7,7]
+     * 输出：[0,0,0,0]
+     */
+//    public static int[] smallerNumbersThanCurrent(int[] nums) {
+//        int[] arr = new int[nums.length];
+//        for (int i = 0; i < nums.length; i++) {
+//            int count = 0;
+//            for (int num : nums) {
+//                if (nums[i] > num) {
+//                    count++;
+//                }
+//            }
+//            arr[i] = count;
+//        }
+//        return arr;
+//    }
+
+    /**
+     * 给你一个整数数组 nums，请你返回其中位数为偶数的数字的个数。
+     * 示例 1：
+     * 输入：nums = [12,345,2,6,7896]
+     * 输出：2
+     * 解释：
+     * 12 是 2 位数字（位数为偶数） 
+     * 345 是 3 位数字（位数为奇数）  
+     * 2 是 1 位数字（位数为奇数） 
+     * 6 是 1 位数字 位数为奇数） 
+     * 7896 是 4 位数字（位数为偶数）  
+     * 因此只有 12 和 7896 是位数为偶数的数字
+     * 示例 2：
+     * 输入：nums = [555,901,482,1771]
+     * 输出：1
+     * 解释：
+     * 只有 1771 是位数为偶数的数字。
+     * 100000
+     */
+//    public static int findNumbers(int[] nums) {
+//        int count = 0;
+//        for (int num : nums) {
+//            if (String.valueOf(num).length() % 2 == 0) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
+
+    /**
+     * 幂集。编写一种方法，返回某集合的所有子集。集合中不包含重复的元素。
+     * <p>
+     * 说明：解集不能包含重复的子集。
+     * <p>
+     * 示例:
+     * <p>
+     * 输入： nums = [1,2,3]
+     * 输出：
+     * [
+     * [3],
+     *   [1],
+     *   [2],
+     *   [1,2,3],
+     *   [1,3],
+     *   [2,3],
+     *   [1,2],
+     *   []
+     * ]
+     */
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> answers = new ArrayList<>();
+        answers.add(new ArrayList<>());
+        //当前元素
+        for (int n : nums) {
+            //原来元素的基础数
+            int length = answers.size();
+            for (int i = 0; i < length; i++) {
+                //将原来的元素集合全部加入新集合中
+                List<Integer> lists = new ArrayList<>(answers.get(i));
+                //将当前元素加入到新集合中
+                lists.add(n);
+                //结果集
+                answers.add(lists);
+            }
+
+        }
+        return answers;
     }
 
+    /**
+     * 给你一个整数数组 nums，请你选择数组的两个不同下标 i 和 j，使 (nums[i]-1)*(nums[j]-1) 取得最大值。
+     * 请你计算并返回该式的最大值。
+     * 示例 1：
+     * 输入：nums = [3,4,5,2]
+     * 输出：12
+     * 解释：如果选择下标 i=1 和 j=2（下标从 0 开始），则可以获得最大值，(nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12 。
+     * 示例 2：
+     * 输入：nums = [1,5,4,5]
+     * 输出：16
+     * 解释：选择下标 i=1 和 j=3（下标从 0 开始），则可以获得最大值 (5-1)*(5-1) = 16 。
+     * 示例 3：
+     * 输入：nums = [3,7]
+     * 输出：12
+     */
+//    public int maxProduct(int[] nums) {
+//        Arrays.sort(nums);
+//        return (nums[nums.length-1]-1)*(nums[nums.length-2]-1);
+//    }
+
     public static void main(String[] args) {
-        kidsWithCandies(new int[]{2, 3, 5, 1, 3}, 3);
+        subsets(new int[]{1, 2, 3});
     }
 }
--- src/main/java/com/chenxd/algorithm/linkedlist/training/LinkedTraining.java
+++ src/main/java/com/chenxd/algorithm/linkedlist/training/LinkedTraining.java
@@ -176,6 +176,45 @@ public class LinkedTraining {
         return slow;
     }
 
+    /**
+     * 判断链表是否有环
+     * 双指针 快慢指针
+     */
+    public static boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 已知链表有环，判断环的位置
+     * 双指针 快慢指针
+     */
+    public static ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
     public static void main(String[] args) {
         ListNode listNode = new ListNode(1);
         listNode.addAtTail(2);
