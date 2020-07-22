c7fc67f95c0623dadb376fe0b4019ac225f41929
xiaodong.chen <xiaodong.chen@huixiaoer.com>
2020-07-22 16:00:10 +0800

    数据结构与算法

---
+++ src/main/java/com/chenxd/algorithm/hash/HashTraining.java
@@ -0,0 +1,196 @@
+package com.chenxd.algorithm.hash;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author create by xiaodong.chen
+ * @create 2020/07/22
+ * @email xiaodong.chen@huixiaoer.com
+ * @description
+ */
+public class HashTraining {
+    /**
+     * 给你一个整数数组 nums 。
+     * 如果一组数字 (i,j) 满足 nums[i] == nums[j] 且 i < j ，就可以认为这是一组 好数对 。
+     * 返回好数对的数目。
+     * 示例 1：
+     * 输入：nums = [1,2,3,1,1,3]
+     * 输出：4
+     * 解释：有 4 组好数对，分别是 (0,3), (0,4), (3,4), (2,5) ，下标从 0 开始
+     * 如果一个数出现n次，则好数对的对数为 n*(n-1)/2
+     */
+    public static int numIdenticalPairs(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int v = entry.getValue();
+            ans += v * (v - 1) / 2;
+        }
+        return ans;
+    }
+
+    /**
+     *  给定字符串J 代表石头中宝石的类型，和字符串 S代表你拥有的石头。 S 中每个字符代表了一种你拥有的石头的类型，你想知道你拥有的石头中有多少是宝石。
+     * J 中的字母不重复，J 和 S中的所有字符都是字母。字母区分大小写，因此"a"和"A"是不同类型的石头。
+     * 示例 1:
+     * 输入: J = "aA", S = "aAAbbbb"
+     * 输出: 3
+     */
+    public static int numJewelsInStones(String J, String S) {
+        Set<Character> set = new HashSet<>();
+        for (char c : J.toCharArray()) {
+            set.add(c);
+        }
+        int count = 0;
+        for (char c : S.toCharArray()) {
+            if (set.contains(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * 给你一个数组 nums，对于其中每个元素 nums[i]，请你统计数组中比它小的所有数字的数目。
+     * 换而言之，对于每个 nums[i] 你必须计算出有效的 j 的数量，其中 j 满足 j != i 且 nums[j] < nums[i] 。
+     * 以数组形式返回答案。
+     * 示例 1：
+     * 输入：nums = [8,1,2,2,3]
+     * 输出：[4,0,1,1,3]
+     * 解释：
+     * 对于 nums[0]=8 存在四个比它小的数字：（1，2，2 和 3）。
+     * 对于 nums[1]=1 不存在比它小的数字。
+     * 对于 nums[2]=2 存在一个比它小的数字：（1）。
+     * 对于 nums[3]=2 存在一个比它小的数字：（1）。
+     * 对于 nums[4]=3 存在三个比它小的数字：（1，2 和 2）。
+     * 示例 2：
+     * 输入：nums = [6,5,4,8]
+     * 输出：[2,1,0,3]
+     * 示例 3：
+     * 输入：nums = [7,7,7,7]
+     * 输出：[0,0,0,0]
+     * in order to improve the time complexity, we can sort the array and get the answer for each array element.
+     */
+    public static int[] smallerNumbersThanCurrent(int[] nums) {
+
+        return new int[0];
+    }
+
+    /**
+     * 给定两个数组，编写一个函数来计算它们的交集。
+     * 示例 1：
+     * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+     * 输出：[2]
+     * 示例 2：
+     * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+     * 输出：[9,4]
+     */
+//    public static int[] intersection(int[] nums1, int[] nums2) {
+//        Set<Integer> set1 = new HashSet<>();
+//        Set<Integer> set2 = new HashSet<>();
+//        for (int i : nums1) {
+//            set1.add(i);
+//        }
+//        for (int i : nums2) {
+//            set2.add(i);
+//        }
+//        set1.retainAll(set2);
+//        return set1.stream().mapToInt(Integer::intValue).toArray();
+//    }
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        for (int i : nums1) {
+            set1.add(i);
+        }
+        Set<Integer> set2 = new HashSet<>();
+        for (int i : nums2) {
+            if (set1.contains(i)) {
+                set2.add(i);
+            }
+        }
+        return set2.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    /**
+     * 给你一个整数数组 arr，请你帮忙统计数组中每个数的出现次数。
+     * 如果每个数的出现次数都是独一无二的，就返回 true；否则返回 false。
+     * 示例 1：
+     * 输入：arr = [1,2,2,1,1,3]
+     * 输出：true
+     * 解释：在该数组中，1 出现了 3 次，2 出现了 2 次，3 只出现了 1 次。没有两个数的出现次数相同。
+     * 示例 2：
+     * 输入：arr = [1,2]
+     * 输出：false
+     * 示例 3：
+     * 输入：arr = [-3,0,1,-3,1,1,1,-3,10,0]
+     * 输出：true
+     */
+    public static boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for (Integer value : map.values()) {
+            if (!set.add(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniqueOccurrences(new int[]{-3,0,1,-3,1,1,1,-3,10,0}));
+    }
+}
+
+class Codec {
+    private Map<Integer, String> map = new HashMap<>();
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        map.put(longUrl.hashCode(), longUrl);
+        return "http://tinyurl.com/" + longUrl.hashCode();
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }
+}
+
+/**
+ * 设计一个方法，找出任意指定单词在一本书中的出现频率。
+ * 你的实现应该支持如下操作：
+ * WordsFrequency(book)构造函数，参数为字符串数组构成的一本书
+ * get(word)查询指定单词在书中出现的频率
+ * 示例：
+ * WordsFrequency wordsFrequency = new WordsFrequency({"i", "have", "an", "apple", "he", "have", "a", "pen"});
+ * wordsFrequency.get("you"); //返回0，"you"没有出现过
+ * wordsFrequency.get("have"); //返回2，"have"出现2次
+ * wordsFrequency.get("an"); //返回1
+ * wordsFrequency.get("apple"); //返回1
+ * wordsFrequency.get("pen"); //返回1
+ */
+class WordsFrequency {
+    private Map<String, Integer> map = new HashMap<>();
+
+    public WordsFrequency(String[] book) {
+        for (String s : book) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+    }
+
+    public int get(String word) {
+        return map.getOrDefault(word, 0);
+    }
+}
---
+++ src/main/java/com/chenxd/algorithm/tree/TreeNode.java
@@ -0,0 +1,18 @@
+package com.chenxd.algorithm.tree;
+
+/**
+ * @author create by xiaodong.chen
+ * @create 2020/07/22
+ * @email xiaodong.chen@huixiaoer.com
+ * @description
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        this.val = x;
+    }
+
+}
