509680d409091a824ada5fce5f0c2a09b08f3922
xiaodong.chen <xiaodong.chen@huixiaoer.com>
2020-07-29 17:47:11 +0800

    数据结构与算法

--- src/main/java/com/chenxd/algorithm/offer/OfferTraining.java
+++ src/main/java/com/chenxd/algorithm/offer/OfferTraining.java
@@ -12,6 +12,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+import java.util.TreeMap;
 
 /**
  * @author create by xiaodong.chen
@@ -233,12 +234,92 @@ public class OfferTraining {
      * 输入：nums = [1,2,10,4,1,4,3,3]
      * 输出：[2,10] 或 [10,2]
      */
+//    public static int[] singleNumbers(int[] nums) {
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int num : nums) {
+//            map.put(num, map.getOrDefault(num, 0) + 1);
+//        }
+//        List<Integer> list = new ArrayList<>();
+//        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+//            if (entry.getValue() == 1) {
+//                list.add(entry.getKey());
+//            }
+//        }
+//        return list.stream().mapToInt(Integer::intValue).toArray();
+//    }
     public static int[] singleNumbers(int[] nums) {
+        int a = 0;
+        for (int num : nums) {
+            a ^= num;
+        }
+        int mask = a & (-a);
         int[] arr = new int[2];
-
+        for (int num : nums) {
+            if ((mask & num) == 0) {
+                arr[0] ^= num;
+            } else {
+                arr[1] ^= num;
+            }
+        }
         return arr;
     }
+
+    /**
+     * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     * 示例:
+     * 现有矩阵 matrix 如下：
+     * [
+     * [1,   4,  7, 11, 15],
+     * [2,   5,  8, 12, 19],
+     * [3,   6,  9, 16, 22],
+     * [10, 13, 14, 17, 24],
+     * [18, 21, 23, 26, 30]
+     * ]
+     * 给定 target = 5，返回 true。
+     * <p>
+     * 给定 target = 20，返回 false。
+     */
+//    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+//        for (int[] ints : matrix) {
+//            if(ints.length == 0){
+//                return false;
+//            }
+//            int left = ints[0];
+//            int right = ints[ints.length - 1];
+//            for (int i = 0; i < ints.length; i++) {
+//                if (left == target) {
+//                    return true;
+//                }
+//                if (right == target) {
+//                    return true;
+//                }
+//                left = ints[i];
+//                right = ints[ints.length - i - 1];
+//            }
+//        }
+//        return false;
+//    }
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int row = 0, column = columns - 1;
+        while (row < rows && column >= 0) {
+            int num = matrix[row][column];
+            if (num == target) {
+                return true;
+            } else if (num > target) {
+                column--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
-        Deque<Integer> deque = new LinkedList<>();
+//        System.out.println(findNumberIn2DArray(new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17,
+//                24}, {18, 21, 23, 26, 30}}, 5));
+        System.out.println(findNumberIn2DArray(new int[][]{{-5}},-5));
     }
 }
--- src/main/java/com/chenxd/algorithm/tree/TreeNode.java
+++ src/main/java/com/chenxd/algorithm/tree/TreeNode.java
@@ -1,5 +1,10 @@
 package com.chenxd.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
 /**
  * @author create by xiaodong.chen
  * @create 2020/07/22
@@ -14,5 +19,167 @@ public class TreeNode {
     TreeNode(int x) {
         this.val = x;
     }
+}
 
+class TestTree {
+
+    private static List<Integer> list = new ArrayList<>();
+
+    /**
+     * 前序遍历
+     *
+     * @param node
+     */
+    public static List<Integer> preOrderTree(TreeNode node) {
+        if (node == null) {
+            return list;
+        }
+        list.add(node.val);
+        preOrderTree(node.left);
+        preOrderTree(node.right);
+        return list;
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode treeNode = stack.pollLast();
+            list.add(treeNode.val);
+            if (treeNode.right != null) {
+                stack.add(treeNode.right);
+            }
+            if (treeNode.left != null) {
+                stack.add(treeNode.left);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * 中序遍历
+     *
+     * @param node
+     */
+    public static void inOrderTree(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        inOrderTree(node.left);
+        System.out.print(node.val + "\t");
+        inOrderTree(node.right);
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode treeNode = stack.pollLast();
+            if (treeNode.right != null) {
+                stack.add(treeNode.right);
+            }
+            if (treeNode.left != null) {
+                stack.add(treeNode.left);
+            }
+            list.add(treeNode.val);
+        }
+        return list;
+    }
+
+    /**
+     * 后序遍历
+     *
+     * @param node
+     */
+    public static void postOrderTree(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        postOrderTree(node.left);
+        postOrderTree(node.right);
+        System.out.print(node.val + "\t");
+    }
+
+    /**
+     * 数的最大深度
+     *
+     * @param node
+     * @return
+     */
+    public static int depth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = depth(node.left);
+        int right = depth(node.right);
+        return left > right ? left + 1 : right + 1;
+    }
+
+    public static int nodeCount(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return nodeCount(node.left) + nodeCount(node.right) + 1;
+    }
+
+    public static int leafNodeCount(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        if (node.left == null && node.right == null) {
+            return 1;
+        }
+        return leafNodeCount(node.left) + leafNodeCount(node.right);
+    }
+
+    /**
+     * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
+     * 例如输入：
+     *      4
+     *    /   \
+     *   2     7
+     *  / \   / \
+     * 1   3 6   9
+     * 镜像输出：
+     *      4
+     *    /   \
+     *   7     2
+     *  / \   / \
+     * 9   6 3   1
+     * 示例 1：
+     * 输入：root = [4,2,7,1,3,6,9]
+     * 输出：[4,7,2,9,6,3,1]
+     */
+    public static TreeNode mirrorTree(TreeNode root) {
+        if(root == null){
+            return root;
+        }
+        TreeNode temp = root.left;
+        root.left = mirrorTree(root.right);
+        root.right = mirrorTree(temp);
+        return root;
+    }
+
+    /**
+     * 前：10 5 2 8 15 12 18
+     * 中：2 5 8 10 12 15 18
+     * 后：2 8 5 12 18 15 10
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        TreeNode treeNode = new TreeNode(4);
+        treeNode.left = new TreeNode(2);
+        treeNode.right = new TreeNode(7);
+
+        treeNode.left.left = new TreeNode(1);
+        treeNode.left.right = new TreeNode(3);
+
+        treeNode.right.left = new TreeNode(6);
+        treeNode.right.right = new TreeNode(9);
+        System.out.println(mirrorTree(treeNode));
+
+    }
 }
